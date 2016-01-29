58c0e03b23f3f23d0302cb9f7f90864ba1262158
cvolion <cvolion@me.com>
2016-01-29 16:21:02 +0000

    Added validations and create action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,23 @@
 class ContactsController < ApplicationController
+    
     def new         # works with app/views/contacts/new.html.erb
         @contact = Contact.new
     end
+    
     def create
+        @contact = Contact.new(contact_params)
+        if @contact.save
+            flash[:success] = "Thank you! An owl has been dispatched with your comment and our goblin staff will follow up appropriately!"
+            redirect_to new_contact_path
+        else
+            flash[:danger] = "Merlin's Beard! You can't send that without filling in all of the required fields!"
+            redirect_to new_contact_path
+        end
     end
+    
+    private
+        def contact_params
+            params.require(:contact).permit(:name, :email, :comments)
+        end
+    
 end
