67bad2869ba97ea0f67aa61f735110d8700ca698
cvolion <cvolion@me.com>
2016-02-18 16:27:02 +0000

    Updated routes.rb with new devise controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   # Generate db routes > preview routes with 'bundle exec rake routes'
   resources :contacts
   # About
